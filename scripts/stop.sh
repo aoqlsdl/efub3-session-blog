a8113986b4139b999d59c8193da33febd799d788
aoqlsdl <bini92444@gmail.com>
2023-05-30 20:48:42 +0900

    🔧fix: 배포 오타 수정

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PROJECT_ROOT="/home/ubuntu/demo"
+PROJECT_ROOT="/home/ubuntu/efub3-session-blog"
 JAR_FILE="$PROJECT_ROOT/spring-webapp.jar"
 
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
